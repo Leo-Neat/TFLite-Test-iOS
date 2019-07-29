87941dc85121c7bb0bcc69f6f976e700c22d0512
Leo Neat <lneat@ucsc.edu>
2019-07-29 11:48:40 -0700

    Initial Commit

---
+++ TFLite-Model-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TFLite-Model-Test
+//
+//  Created by Leo Neat on 7/29/19.
+//  Copyright © 2019 Leo Neat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TFLite-Model-TestTests/TFLite_Model_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  TFLite_Model_TestTests.swift
+//  TFLite-Model-TestTests
+//
+//  Created by Leo Neat on 7/29/19.
+//  Copyright © 2019 Leo Neat. All rights reserved.
+//
+
+import XCTest
+@testable import TFLite_Model_Test
+
+class TFLite_Model_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
